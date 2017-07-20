13c1e7ce2338938ff5c401fd1fc28044c0285ad2
Deployment Server <adriaan.pelzer@trinitymirror.com>
2017-07-20 14:55:56 +0000

    Init

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    queryObject: require ( './lib/queryObject.js' ),
+    itemStore: require ( './lib/itemStore.js' ),
+    itemGenerator: require ( './lib/itemGenerator.js' )
+};
